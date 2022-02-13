dd4286d648b4e82dfe930cf5bdc461657163af3f
Nikhil <Nikhil@DESKTOP-15QTNJ9>
2022-02-13 17:32:10 +0530

    By me

---
+++ GitHub Practice/src/com/demo/DemoGit.java
@@ -0,0 +1,22 @@
+package com.demo;
+
+public class DemoGit {
+	
+	public void m1()
+	{
+		System.out.println("This is GitHub for M1---");
+	}
+	
+	public void m2()
+	{
+		System.out.println("This is GitHub for M2---");
+	}
+	
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is Final Program");
+		
+		
+	}
+}
